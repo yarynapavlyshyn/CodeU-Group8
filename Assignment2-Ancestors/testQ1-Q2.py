42e3bdcac84bef14b27d6d151a318bf1e9d50f60
yarynapavlyshyn <pavlyshyn_y@ucu.edu.ua>
2017-05-29 23:34:02 -0400

    Update testQ1-Q2.py

--- Assignment2-Ancestors/testQ1-Q2.py
+++ Assignment2-Ancestors/testQ1-Q2.py
@@ -8,58 +8,37 @@ class Test_Q1_and_Q2(unittest.TestCase):
     Tests for ancestors_by_keys() function in Q1, find_ancestors() function
     will be tested automaticaly as a part of the first one
     """
-    def test_ancestors_by_keys_empty_tree(self):
+    def setUp(self):
+        self.emptyTree = BinaryTree()
         t = BinaryTree()
-        self.assertIsNone(ancestors_by_keys(t, 6))
+        values = [1, 2, 3, "o", "k"]
+        for value in values:
+            t.add(value)
+        self.notEmptyTree = t
+            
+    def test_ancestors_by_keys_empty_tree(self):
+        self.assertIsNone(ancestors_by_keys(self.emptyTree, 6))
 
     def test_ancestors_by_keys_if_not_exists(self):
-        t = BinaryTree()
-        values = [1, 2, 3, "o", "k"]
-        for value in values:
-            t.add(value)
-        self.assertIsNone(ancestors_by_keys(t, 5))
+        self.assertIsNone(ancestors_by_keys(self.notEmptyTree, 5))
 
-    def test_ancestors_by_keys_if_exists_int(self):
-        t = BinaryTree()
-        values = [1, 2, 3, "o","k"]
-        for value in values:
-            t.add(value)
-        self.assertEqual([1], ancestors_by_keys(t, 3))
-
-    def test_ancestors_by_keys_if_exists_str(self):
-        t = BinaryTree()
-        values = [1, 2, 3, "o", "k"]
-        for value in values:
-            t.add(value)
-        self.assertEqual([2, 1], ancestors_by_keys(t, "o"))
+    def test_ancestors_by_keys_if_exists(self):
+        self.assertEqual([2, 1], ancestors_by_keys(self.notEmptyTree, "o"))
 
     """
     Tests for lowest_common_ancestor() function in Q2
     """
     def test_lowest_common_ancestor_empty_tree(self):
-        t = BinaryTree()
-        self.assertIsNone(lowest_common_ancestor(t, 8, 6))
+        self.assertIsNone(lowest_common_ancestor(self.emptyTree, 8, 6))
 
     def test_ancestors_by_keys_if_not_exists(self):
-        t = BinaryTree()
-        values = [1, 2, 3, "o", "k"]
-        for value in values:
-            t.add(value)
-        self.assertIsNone(lowest_common_ancestor(t, "o", "z"))
+        self.assertIsNone(lowest_common_ancestor(self.notEmptyTree, "o", "z"))
 
-    def test_ancestors_by_keys_if_exists_but_root(self):
-        t = BinaryTree()
-        values = [1, 2, 3, "o","k"]
-        for value in values:
-            t.add(value)
-        self.assertIsNone(lowest_common_ancestor(t, 1, 3))
+    def test_ancestors_by_keys_if_exists_but_is_root(self):
+        self.assertIsNone(lowest_common_ancestor(self.notEmptyTree, 1, 3))
 
     def test_ancestors_by_keys_if_exists_str(self):
-        t = BinaryTree()
-        values = [1, 2, 3, "o", "k"]
-        for value in values:
-            t.add(value)
-        self.assertEqual(2, lowest_common_ancestor(t, "o", "k").key)
+        self.assertEqual(2, lowest_common_ancestor(self.notEmptyTree, "o", "k").key)
 
 if __name__ == '__main__':
     unittest.main()
