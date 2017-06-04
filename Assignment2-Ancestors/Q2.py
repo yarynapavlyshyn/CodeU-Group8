d75aad33b9f03d018eae9b494b42fc770462ed86
yarynapavlyshyn <pavlyshyn_y@ucu.edu.ua>
2017-06-04 11:32:21 +0300

    Update Q2.py
    The function last_common_element() could be much more easier! I've fixed the algorithm and then wrote a new one that is a little bit simpler.
    (I had some extra stuff there and a mistake in indexes of course)

--- Assignment2-Ancestors/Q2.py
+++ Assignment2-Ancestors/Q2.py
@@ -13,8 +13,7 @@ def lowest_common_ancestor(tree, key1, key2):
     ancestors1 = find_ancestors(tree, key1)
     ancestors2 = find_ancestors(tree, key2)
     if ancestors1 is None or ancestors2 is None: return None
-    common_anc = last_common_element(ancestors1, ancestors2)
-    return common_anc
+    return last_common_element(ancestors1, ancestors2)
 
 def last_common_element(list1, list2):
     """
@@ -23,14 +22,22 @@ def last_common_element(list1, list2):
     :param list2: list(*)
     :return: *
     """
-    result = None
+    # Here is fixed first version just not to live it wrong, but the new one below is simpler
+    """
     max_i = min(len(list1), len(list2))
+    if not max_i:return
     for i in range(max_i):
-        result = list1[-i-1]
-        if list1[-i-1] != list2[i]:
-            result = list1[-i]
-            break
-    return result
+        if list1[-i-1] != list2[-i-1]:
+            return list1[-i]
+    return list1[- max_i]
+    """
+    
+    max_i = min(len(list1), len(list2))
+    if not max_i: return   
+    i = 1
+    while list1[-i] == list2[-i] and i < max_i:
+        i += 1
+    return list1[-i]
 
 def process_and_print_out_LCA():
     elements_for_tree = input("Print the values separated by the TWO whitespaces: ").split("  ")
